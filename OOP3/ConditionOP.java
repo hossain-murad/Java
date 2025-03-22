c41eed30f2b26a6b7e3a5f5d8cc3497ec67cae71
hossain-murad <dev.hossainmurad@gmail.com>
2025-03-22 15:50:24 +0900

    add new files

---
+++ OOP3/ConditionOP.java
@@ -0,0 +1,10 @@
+public class ConditionOP {
+  public static void main(String[] args) {
+    int num1 = 10;
+    int num2 = 20;
+
+    int max = (num1 > num2) ? num1 : num2;
+
+    System.out.println("Max value is: " + max);
+  }
+}
