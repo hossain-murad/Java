22bf86717b8d7c23e9e5ff835c646377c9289dbe
hossain-murad <dev.hossainmurad@gmail.com>
2025-03-22 14:26:40 +0900

    add new files

---
+++ OOP3/Comp.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Comp {
+  public static void main(String[] args) {
+    String s1 = "Hello";
+    String s2 = s1.toLowerCase();
+    String s3 = "hello";
+
+    if(s2.compareTo(s3)== 0)
+    System.out.println("Equal!");
+  }
+}
