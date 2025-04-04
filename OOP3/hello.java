bf8e5961f9325e526ed146b4ad6ef7e60ea3bd95
hossain-murad <dev.hossainmurad@gmail.com>
2025-04-04 22:28:09 +0900

    modified hello.java

--- OOP3/hello.java
+++ OOP3/hello.java
@@ -1,7 +1,7 @@
-public class hello{
-  public static void main(String[] args){
+public class hello {
+  public static void main(String[] args) {
     int a = 10, b = 20;
-    int c = (a+b);
-    System.out.println("Addition :"+ c);
+    int c = (a + b);
+    System.out.println("Addition :" + c);
   }
-} 
+}
