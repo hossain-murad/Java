a3f9e72b42bef565da9248d6e980dfb5451cd01f
hossain-murad <dev.hossainmurad@gmail.com>
2025-04-16 13:28:00 +0900

    added Wrapper.java

---
+++ OOP3/Wrapper.java
@@ -0,0 +1,14 @@
+public class Wrapper {
+  public static void main(String[] args) {
+    Integer n = 7;
+    Integer f = 10;
+
+    Double d = n.doubleValue();
+    int v = 4;
+    Character myChar = 'A';
+    System.out.println("The value of n is: " + n);
+    System.out.println("The value of f as double: " + f.doubleValue());
+    System.out.println("The value of d is: " + d);
+    System.out.println("The value of n as a string: " + n.toString());
+  }
+}
